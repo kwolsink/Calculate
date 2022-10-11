5ddb5cba36a4fd163b5303d06c2b1909514ed560
Kasper <kwolsink@gmail.com>
2022-10-11 17:45:09 +0200

    write test function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,11 @@
 import * as functions from "firebase-functions";
 
+import { initializeApp, firestore } from "firebase-admin";
+
+
+
+const admin = initializeApp()
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -7,3 +13,27 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const generateExcercise = functions.https.onCall(async (data, context) => {
+    if (!context.auth) {
+        throw new functions.https.HttpsError('failed-precondition', 'This action can only be performed while authenticated.')
+    }
+
+    const db = admin.firestore()
+
+    const uuid = context.auth.uid
+
+    const excercise = {
+        passed: false,
+        body: {
+            equation: "4 + 5",
+            answer: 9
+        }
+    }
+
+    const docRef = await db.collection('users').doc(uuid).collection('excercises_arithmethic').add(excercise)
+    return await docRef.update({
+        timestamp: firestore.FieldValue.serverTimestamp()
+    })
+})
+
